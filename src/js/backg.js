682dc981f66787b983246950ee12e1e132dd7978
zomby43 <psycho432@gmail.com>
2023-03-22 15:28:34 -0300

    v2.6 js update

--- src/js/backg.js
+++ src/js/backg.js
@@ -1,11 +1,19 @@
+var textArray = ["bievenido", "welcome", "أهلا بك", "환영", "ようこそ", "добро пожаловать", "välkommen",
+    "bienvenue", "bem-vinda", "欢迎", "chào mừng"]; // Array
+var currentTextIndex = 0; // initial index
 
+function changeText() {
+	// Get the element where the text will be displayed
+	var textElement = document.getElementById("text");
 
-function random (){
-    
-const arr = ["one", "two", "three"];
-const random = arr[Math.floor(Math.random() * arr.length)];
-console.log(random);
+	// Display the current text
+	textElement.innerHTML = textArray[currentTextIndex];
 
+	// Increment the index and wrap around if needed
+	currentTextIndex = (currentTextIndex + 1) % textArray.length;
+
+	// Wait for 2 seconds
+	setTimeout(changeText, 2000);
 }
 
 
