7332a6592006941034032d22cb9283f3eeafadf7
zomby43 <psycho432@gmail.com>
2023-05-12 03:47:26 -0400

    v4 dark mode

---
+++ src/js/darkModeToggle.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('darkModeToggle').addEventListener('click', function() {
+        document.body.classList.toggle('dark');
+    });
+});
+
