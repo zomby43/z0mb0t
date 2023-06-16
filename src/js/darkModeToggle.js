29da8a3a3b40df326eb569308605574424311ce6
zomby43 <psycho432@gmail.com>
2023-06-16 00:03:59 -0400

    v4.8 dark mode fix

--- src/js/darkModeToggle.js
+++ src/js/darkModeToggle.js
@@ -1,6 +1,23 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+
+    // Comprueba si el modo oscuro estaba habilitado en la última visita del usuario.
+    if (localStorage.getItem('darkMode') === 'true') {
+        document.body.classList.add('dark');
+    } else {
+        document.body.classList.remove('dark');
+    }
+
+    // Agrega un evento de clic al botón de modo oscuro.
     document.getElementById('darkModeToggle').addEventListener('click', function() {
-        document.body.classList.toggle('dark');
+
+        // Si el modo oscuro está habilitado, desactívalo. Si no, actívalo.
+        if (document.body.classList.contains('dark')) {
+            document.body.classList.remove('dark');
+            localStorage.setItem('darkMode', 'false');
+        } else {
+            document.body.classList.add('dark');
+            localStorage.setItem('darkMode', 'true');
+        }
     });
 });
 
